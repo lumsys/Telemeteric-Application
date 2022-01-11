3e773c3ed413422b043952b86bbd0613e3f71c67
lumsys <sunday4oged@yahoo.com>
2022-01-11 22:06:44 +0100

    procFr

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 
 
 //middleware
